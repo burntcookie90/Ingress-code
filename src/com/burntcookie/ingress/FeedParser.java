29bd07e400e9e593c01f79054066153bf54afe65
Vishnu Rajeevan <Vishnu Rajeevan>
2012-12-07 17:20:49 -0500

    first repo commit

---
+++ src/com/burntcookie/ingress/FeedParser.java
@@ -0,0 +1,6 @@
+package com.burntcookie.ingress;
+import java.util.List;
+
+public interface FeedParser {
+	List<Message> parse();
+}
---
+++ src/com/burntcookie/ingress/FeedParserFactory.java
@@ -0,0 +1,22 @@
+package com.burntcookie.ingress;
+public abstract class FeedParserFactory {
+	static String feedUrl = "http://www.reddit.com/r/ingress/new/.rss?sort=new";
+	
+	public static FeedParser getParser(){
+		return getParser(ParserType.ANDROID_SAX);
+	}
+	
+	public static FeedParser getParser(ParserType type){
+		switch (type){
+			case SAX:
+				return new SaxFeedParser(feedUrl);
+			case DOM:
+				return new DomFeedParser(feedUrl);
+			case ANDROID_SAX:
+				return new AndroidSaxFeedParser(feedUrl);
+			case XML_PULL:
+				return new XmlPullFeedParser(feedUrl);
+			default: return null;
+		}
+	}
+}
